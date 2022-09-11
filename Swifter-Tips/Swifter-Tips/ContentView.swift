2505bac886f7c70863324bc2955d59e0c83ce384
linxiaohai <linxh03@flatincbr.com>
2022-09-11 22:36:52 +0800

    [+]init project

---
+++ Swifter-Tips/Swifter-Tips/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swifter-Tips
+//
+//  Created by linxiaohai on 2022/9/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swifter-Tips/Swifter-Tips/Swifter_TipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swifter_TipsApp.swift
+//  Swifter-Tips
+//
+//  Created by linxiaohai on 2022/9/11.
+//
+
+import SwiftUI
+
+@main
+struct Swifter_TipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
